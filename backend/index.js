f297270ef443f53c54f15f57a7e0eecc4dd11810
Danish84295 <danishahmad01092005@gmail.com>
2026-07-07 23:33:15 +0530

    Fix Vite production build path

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,40 @@
-const dns = require("node:dns");
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const path = require('path');
 
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-const express =require('express');
-const cors =require("cors");
-const dotenv=require("dotenv");
-const connectDB =require('./config/db');
 dotenv.config();
 connectDB();
 
+const app = express();
 
+// Set CORS for frontend URL / allow single-node deploy
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL],
+  credentials: true
+}));
 
-const app=express();
-app.use(cors(
-    {
-        origin:['http://localhost:5173' , 'http://127.0.0.1:5173'],
-        credentials:true
-    }
-));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
-app.get("/",(req,res)=>{
-    res.send("DanCart backend is working");
-});
-
-app.use('/api/auth',require('./routes/authRoutes'));
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/orders',require('./routes/orderRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
-app.use('/api/analytics',require('./routes/analyticsRoutes'));
+app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT =process.env.PORT || 5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('ShopNest API is running in Development mode...');
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
